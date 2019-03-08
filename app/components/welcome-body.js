3e5e0596643bb9642805ec77e705d2ab875bd945
Timoshenko Alexandr <thick.tav@gmail.com>
2019-03-08 21:28:15 +0100

    general ui for welcome

---
+++ app/components/welcome-body.js
@@ -0,0 +1,24 @@
+import Component from '@ember/component';
+import $ from 'jquery';
+
+export default Component.extend({
+  init() {
+    this._super(arguments)
+    $(window).on('resize', this.handleSize.bind(this));
+  },
+  willDestroyElement() {
+    this._super(...arguments);
+    $(window).off("resize");
+  },
+  handleSize() {
+    let minHeight = 700;
+    let calcHeight = Math.max($(window).height(), minHeight) - $('.navbar').height() - $('footer').height()
+    $(this.element).css('height', calcHeight)
+  },
+  didInsertElement() {
+    this._super(...arguments);
+    this.handleSize()
+
+  },
+
+});

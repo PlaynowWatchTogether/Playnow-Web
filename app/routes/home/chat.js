4adc62e1abd3d94943f499d278a1e147218d8743
Timoshenko Alexandr <thick.tav@gmail.com>
2019-03-10 01:48:47 +0100

    before update ember

--- app/routes/home/chat.js
+++ app/routes/home/chat.js
@@ -2,6 +2,12 @@ import Route from '@ember/routing/route';
 import Ember from "ember";
 
 export default Route.extend({
+  model(params) {
+    return Ember.RSVP.hash({
+      chat: this.store.peekRecord('friends', params['chat_id']),
+      mode: params['mode']
+    });
+  },
   activate() {
     this._super(...arguments);
     Ember.$('body').addClass('chat');

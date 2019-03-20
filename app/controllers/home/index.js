75ae2c99a6a3d40f115222d2c262dbfa5a4627ef
Timoshenko Alexandr <thick.tav@gmail.com>
2019-03-20 01:57:04 +0100

    filter public rooms

--- app/controllers/home/index.js
+++ app/controllers/home/index.js
@@ -1,9 +1,18 @@
 import Controller from '@ember/controller';
 import {inject as service} from '@ember/service';
-
+import {computed} from '@ember/object'
 export default Controller.extend({
   db: service(),
   init() {
     this._super(...arguments);
-  }
+  },
+  filteredModel: computed('model', 'roomQuery', function () {
+    let q = this.get('roomQuery');
+    return this.get('model').filter((elem) => {
+      if (!q || q.length === 0)
+        return true;
+      let title = elem.get('videoName');
+      return title.toLowerCase().includes(q.toLowerCase());
+    })
+  })
 });

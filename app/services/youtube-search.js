fb36f4a4116fddd88f08c38c22ae458b99e1bd24
Timoshenko Alexandr <thick.tav@gmail.com>
2019-04-07 01:47:03 +0200

    before change urls

--- app/services/youtube-search.js
+++ app/services/youtube-search.js
@@ -38,7 +38,7 @@ export default Service.extend({
     return new Promise((resolve) => {
       let category = music ? "&videoCategoryId=10" : '';
       let pageQ = page ? '&pageToken=' + page : '';
-      $.getJSON(SEARCH_URL + '?' + API_KEY + '&part=id,snippet&type=video&videoEmbeddable=true&maxResults=25&q=' + q + category + pageQ, null, (data) => {
+      $.getJSON(SEARCH_URL + '?' + API_KEY + '&part=id,snippet,statistics&type=video&videoEmbeddable=true&maxResults=25&q=' + q + category + pageQ, null, (data) => {
         debug(data);
         resolve({
           nextPage: data['nextPageToken'],
@@ -57,7 +57,7 @@ export default Service.extend({
     return new Promise((resolve) => {
       let category = music ? "&videoCategoryId=10" : '';
       let pageQ = page ? '&pageToken=' + page : '';
-      $.getJSON(VIDEOS_URL + '?' + API_KEY + '&part=id,snippet&type=video&videoEmbeddable=true&chart=mostPopular&maxResults=25' + category + pageQ, null, (data) => {
+      $.getJSON(VIDEOS_URL + '?' + API_KEY + '&part=id,snippet,statistics&type=video&videoEmbeddable=true&chart=mostPopular&maxResults=25' + category + pageQ, null, (data) => {
         debug(data);
         resolve({
           nextPage: data['nextPageToken'],

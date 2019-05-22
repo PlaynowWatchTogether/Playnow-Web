7c234026376eda48ad0b8abd5e444de6bc34925b
Timoshenko Alexandr <thick.tav@gmail.com>
2019-05-22 11:25:31 +0200

    fixes 4

--- app/components/chat-members.js
+++ app/components/chat-members.js
@@ -1,5 +1,11 @@
 import Component from '@ember/component';
-
+import $ from 'jquery';
 export default Component.extend({
-  classNameBindings: ['fullscreen:fullscreen']
+  classNameBindings: ['fullscreen:fullscreen'],
+  didInsertElement(){
+    this._super(...arguments);
+    $(this.element).on('click','.member', (event)=>{
+      $(event.target).toggleClass('large');
+    });
+  }
 });
